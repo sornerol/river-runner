751292ecb67d1bedf6e063c790e04ba4760bf50d
Loren Jamison <loren.jamison@gmail.com>
2022-05-19 19:06:21 -0500

    Wait until visible on screen to start moving.

--- river-runner/scripts/EnemyBase.cs
+++ river-runner/scripts/EnemyBase.cs
@@ -11,13 +11,19 @@ public class EnemyBase : KinematicBody2D
 
     private bool directionFlipped;
 
+    private bool isMoving;
+
     public override void _Ready()
     {
         SetCollisionMaskBit(TERRAIN_MASK_BIT, isAquaticVehicle);
+        isMoving = false;
     }
 
     public override void _PhysicsProcess(float delta)
     {
+        if (!isMoving) {
+            return;
+        }
         base._PhysicsProcess(delta);
         Vector2 movement = new Vector2(getHorizontalMovement(delta), 0);
         KinematicCollision2D collision =  MoveAndCollide(movement);
@@ -37,6 +43,10 @@ public class EnemyBase : KinematicBody2D
 
     public void _OnScreenExited() {
         QueueFree();
+    }
+
+    public void _OnScreenEntered() {
+        isMoving = true;
     }
 
     private float getHorizontalMovement(float delta)
